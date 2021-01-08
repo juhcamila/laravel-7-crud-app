92837cbeb631341b840539a30868c4e429d3599b
juh camila <juhcamila15@windowslive.com>
2021-01-08 00:02:47 -0300

    Crud Contact em php e Laravel

---
+++ app/Models/Contact.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'city',
+        'country',
+        'job_title'       
+    ];
+}
